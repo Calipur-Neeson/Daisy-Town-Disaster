f9c084180f453efa5ce0c3b65c09b4bdc68ede68
Calipur <mycroftn@163.com>
2025-09-07 21:10:12 +0300

    (+)Add Player controller
    move, jump, look
    TODO: sprint

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : NetworkBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpHeight = 2f;
     [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float gravity = -9.81f;
 
     private InputActionMap _actionMap;
     private InputAction _moveAction;
@@ -18,44 +20,125 @@ public class PlayerController : NetworkBehaviour
     private InputAction _lookAction;
 
     private CharacterController controller;
-    public bool isSprinting;
     private Vector2 moveInput = Vector2.zero;
-    private void OnEnable()
+    private Vector2 lookInput = Vector2.zero;
+    
+    private Vector3 velocity;
+    private bool isGrounded;
+    public bool isSprinting;
+    private float xRotation = 0f;
+    
+    private void Awake()
     {
-        var playerMap = actionAsset.FindActionMap("Player");
-        if (playerMap != null)
+        controller = GetComponent<CharacterController>();
+    }
+    
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
         {
-            playerMap.Enable();
-        }
-        else
-        {
-            Debug.LogWarning("InputActionMap 'Player' not found. Please check.");
-        }
+            _actionMap = actionAsset.FindActionMap("Player");
 
+            if (_actionMap != null)
+            {
+                _moveAction = _actionMap.FindAction("Move");
+                _jumpAction = _actionMap.FindAction("Jump");
+                _lookAction = _actionMap.FindAction("Look");
+
+                _actionMap.Enable();
+
+                if (_moveAction != null)
+                {
+                    _moveAction.performed += OnPlayerMove;
+                    _moveAction.canceled += OnPlayerStop;
+                }
+                if (_lookAction != null)
+                {
+                    _lookAction.performed += OnPlayerLook;
+                    _lookAction.canceled += ctx => lookInput = Vector2.zero;
+                }
+                if (_jumpAction != null)
+                {
+                    _jumpAction.performed += ctx => Jump();
+                }
+            }
+            else
+            {
+                Debug.LogWarning("InputActionMap 'Player' not found. Please check.");
+            }
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    
+    private void OnDisable()
+    {
         if (_moveAction != null)
         {
-            _moveAction.performed += OnPlayerMove;
-            _moveAction.canceled += OnPlayerStop;
+            _moveAction.performed -= OnPlayerMove;
+            _moveAction.canceled -= OnPlayerStop;
         }
-        else
+        if (_lookAction != null)
         {
-            Debug.LogWarning("Can not find Move action.");
+            _lookAction.performed -= OnPlayerLook;
+        }
+    }
+    
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        HandleMovement();
+        HandleLook();
+    }
+
+    private void HandleMovement()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
         }
 
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleLook()
+    {
+        transform.Rotate(Vector3.up * lookInput.x * mouseSensitivity);
+        
+        xRotation -= lookInput.y * mouseSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        if (cameraTransform != null)
+        {
+            cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
     }
     private void OnPlayerMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
+    
     private void OnPlayerStop(InputAction.CallbackContext context)
     {
         moveInput = Vector2.zero;
     }
+    
+    private void OnPlayerLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
 
     private void Jump()
     {
-
+        if (isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
     }
-
-
 }
